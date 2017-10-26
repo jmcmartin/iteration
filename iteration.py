3e78456759d6b594fd2cb216a5f019ebbeb6ffc6
jmcmartin <joshuamcm1@gmail.com>
2017-10-25 18:51:28 -0600

    Add averaging functions for hw

--- iteration.py
+++ iteration.py
@@ -49,3 +49,32 @@ def maximum(numbers):
 			current_max = n
 
 	return current_max
+
+def average(scores):
+	total = 0
+	for n in scores:
+		total += n
+
+	return total / len(scores)
+
+def average_without_bottom_two_scores(scores):
++    current_min = scores[0]
++    current_min_placement = -1
++    for n in scores:
++        if n <= current_min:
++            current_min = n
++
++    scores.remove(current_min)
++    current_min2 = scores[0]
++
++    for n in scores:
++        if n <= current_min2:
++            current_min2 = n
++    
++    scores.remove(current_min2)
++    
++    sum_without_bottom_two_scores = 0
++    for n in scores:
++        sum_without_bottom_two_scores += n
++
++    return sum_without_bottom_two_scores / len(scores)
