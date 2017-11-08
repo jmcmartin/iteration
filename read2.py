c2ca9404c2abd803e39e49c5a223aa913768b5ec
Josh McMartin <mcmartinjd@s.dcsdk12.org>
2017-11-08 07:59:14 -0700

    Find most popular color of car

--- read2.py
+++ read2.py
@@ -4,7 +4,10 @@ import sys
 data = open("cars.csv", "r")
 lines = data.readlines()
 
+
 #Number 0
+
+
 list_of_makes_and_models_2006 = []
 
 for i in range(1, len(lines)):
@@ -20,7 +23,10 @@ print "List of all car makes and models from the year 2006:"
 print list_of_makes_and_models_2006
 print "~~~~~~"
 
+
 #Number 1
+
+
 any_year_test = "1999"
 any_year_list = []
 
@@ -36,7 +42,9 @@ for i in range(1, len(lines)):
 print "List of all makes and models from any given year:"
 print any_year_list
 
+
 #Number 2
+
 
 toyotas_since_2000_years = ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"]
 toyotas_list_since_2000 = []
@@ -52,10 +60,32 @@ print "~~~~~~"
 print "Toyotas in list since 2000:"
 print toyotas_list_since_2000
 
+
 #Number 3
 
 
 print "~~~~~~"
 print "Most popular color of car in the list:"
 
+list_of_colors = []
+color_counter = {}
+
+for i in range(1, len(lines)):
+	info = lines[i].rstrip().split(",")
+
+	list_of_colors.append(info[4])
+
+
+for color in list_of_colors:
+	if color in color_counter:
+		color_counter[color] += 1
+	else:
+		color_counter[color] = 1
+
+popular_colors = sorted(color_counter, key = color_counter.get, reverse = True)
+
+most_popular_color = popular_colors[:1]
+
+print most_popular_color
+#print list_of_colors
 print "~~~~~~"
