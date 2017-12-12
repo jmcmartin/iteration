10df701167aceb6a9eed43630122a74b05761e6d
Josh McMartin <mcmartinjd@s.dcsdk12.org>
2017-12-12 08:03:27 -0700

    Fix numbers 1 and 2 on swap

--- swap.py
+++ swap.py
@@ -1,14 +1,15 @@
 #Number 1
 
-new_phrase = []
 
 def replace_letter(phrase, swapped_letter, final_letter):
+	new_phrase = ''
+
 	for i in phrase:
 		if swapped_letter == i:
-			new_phrase.append(final_letter)
+			new_phrase += final_letter
 
 		else:
-			new_phrase.append(i)
+			new_phrase += i
 
 	return new_phrase
 
@@ -18,18 +19,19 @@ print replace_letter("banana", "a", "!")
 	
 #Number 2
 
-new_phrase2 = []
 
 def switch_letters(phrase, swapped_letter_1, swapped_letter_2):
+	new_phrase2 = ''
+
 	for i in phrase:
 		if swapped_letter_1 == i:
-			new_phrase2.append(swapped_letter_2)
+			new_phrase2 += swapped_letter_2
 
 		elif swapped_letter_2 == i:
-			new_phrase2.append(swapped_letter_1)
+			new_phrase2 += swapped_letter_1
 
 		else:
-			new_phrase2.append(i)
+			new_phrase2 += i
 
 	return new_phrase2
 
@@ -80,15 +82,14 @@ print switch_words("The quick brown fox jumps over the lazy dog", "fox", "dog")
 #Number 4
 	
 
-list_of_censored_words = ["fuck", "shit", "ass", "bitch"]
-replacement_words = ["heck", "poop", "butt", "friend"]
-
-def censor_text(bad_sentence):
+def censor_text(bad_sentence, list_of_censored_words, replacement_words):
 	good_sentence = ""
 	check_words = split_sentence(bad_sentence)
+	
 	for i in range(len(check_words)):
 		if check_words[i] not in list_of_censored_words:
 			good_sentence += check_words[i] + ' '
+		
 		for n in range(len(list_of_censored_words)):
 			if check_words[i] == list_of_censored_words[n]:
 				good_sentence += replacement_words[n] + ' '
@@ -96,4 +97,4 @@ def censor_text(bad_sentence):
 	return good_sentence
 
 	
-print censor_text("What the fuck is this shit you bitch ass")
+print censor_text("What the fuck is this shit you bitch ass", ["fuck", "shit", "ass", "bitch"], ["heck", "poop", "butt", "friend"])
