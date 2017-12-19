925aa67d5d99785f43db44e9d20d955f32c75253
Josh McMartin <mcmartinjd@s.dcsdk12.org>
2017-12-19 09:47:50 -0700

    Update transposition

--- transposition.py
+++ transposition.py
@@ -1,5 +1,8 @@
 # https://www.braceyourself.io/apcsp/transposition/
-# https://tabs.ultimate-guitar.com/tab/foreigner/juke_box_hero_chords_1794057?simplified=1
+# https://tabs.ultimate-guitar.com/tab/scorpions/rock_you_like_a_hurricane_chords_668765?simplified=1
+
+scale = ["A", "A#", "B", "C", "C#", "D", "d#", "E", "F", "F#", "G", "G#"]
+
 
 def keyshift(scale, chord, movement):
 	chord_key = scale.index(chord)
@@ -13,7 +16,7 @@ def transpose_song(song_info, scale, movement):
 	
 	for i in range(len(song_info)):
 		if song_info[i] != ',' and song_info[i] != '\n' and song_info != "''":
-			wod += song_info[i]
+			word += song_info[i]
 		else:
 			split_list.append(word)
 			word = ''
@@ -29,7 +32,7 @@ def transpose_song(song_info, scale, movement):
 	transposed = ""
 	for d in range(len(new_list)):
 		if new_list[d] in scale:
-			if new_list[d-1] not in scale:
+			if new_list[d - 1] not in scale:
 				transposed += '\n'
 				transposed += new_list[d]
 			else:
@@ -37,76 +40,116 @@ def transpose_song(song_info, scale, movement):
 		else:
 			transposed += new_list[d]
 			transposed += '\n'
+	
 	return transposed
 
-transpose_song()
 
 
-lyrics_and_cords_original = """
-				   E
-1. Standing in the rain with his head hung low.
-                                         D  
-Couldn't get a ticket, it was a sold out show.
-                      E                           D  
-Heard the roar of the crowd, he could picture the scene.
-                   E                        D  
-Put his ear to the wall, and like a distant scream.
-                E                   D
-He heard one guitar, just blew him away.
-                 E                       D
-Saw stars in his eyes and the very next day.
+song = """
 
-                    E                           D  
-2. Bought a beat up six-string in a second hand store.
-                   E                        D  
-Didn't know how to play it but he knew for sure.
-                 E                    D
-That that one guitar felt good in his hands.
-            E            D
-Didn't take long to understand.
-            E                   D
-Just one guitar, slung way down low.
-              E                       D
-Was a one way ticket, only one way to go.
+[Verse]
 
-Bridge:
-              B
-So he started rocking, ain't never gonna stop.
-              B
-Gotta keep on rocking, someday he's gonna make it to the top.
+E          C
+It's early morning
+    E         A
+The sun comes out
+     E         C
+Last night was shaking
+    E      A
+And pretty loud
+   E      C
+My cat is purring
+    E            A
+And scratches my skin
+   E       C
+So what is wrong
+  E       A
+With another sin
 
-Refr.:
-         Em       G         D            Em
-And be a juke box hero, got stars in his eyes.
-       Em       G        D           Em
-He's a juke box hero. He took one guitar
-         G         D            Em
-Juke box hero, saw stars in his eyes.
-             G             D      E
-Juke box hero, he'll come alive tonight.
+    E        C
+The bitch is hungry
+    E        A
+She needs to tell
+   E        C
+So give her inches
 
-                       E                    D  
-3. In a town without a name, in a heavy downpour.
-                          E                       D  
-Thought he passed his own shadow by the backstage door.
-                        E                     D  
-Like a trip through the past, that day in the rain.
-               E                    D  
-That one guitar made his whole life change.
 
-Bride
-              B
-So he started rocking, ain't never gonna stop.
-              B
-Gotta keep on rocking, someday he's gonna make it to the top.
+    E        A
+And feed her well
 
-Refr.:
-         Em       G         D            Em
-And be a juke box hero, got stars in his eyes.
-       Em       G        D           Em
-He's a juke box hero. He took one guitar
-         G         D            Em
-Juke box hero, saw stars in his eyes.
-             G             D      E
-Juke box hero, he'll come alive tonight.
+     E       C
+More days to come
+    E         A
+New places to go
+     E      C
+I've got to leave
+     E          A
+It's time for a show
+
+
+[Chorus]
+
+E         G A    
+Here I am,     
+C            D       E         G A C D
+Rock you like a hurricane
+E         G A    
+Here I am,     
+C            D       E         G A C D C
+Rock you like a hurricane
+
+
+[Verse]
+
+   E            C
+My body is burning
+   E           A
+It starts to shout
+E            C
+Desire is coming
+   E              A
+It breaks out loud
+
+E            C
+Lust is in cages
+           E                   A
+Till the storm breaks loose
+     E            C
+Just have to make it
+     E         A
+With someone I choose
+
+    E        C
+The night is calling
+  E       A
+I have to go
+    E       C
+The wolf is hungry
+   E       A
+He runs to show
+
+     E           C
+He's licking his lips
+     E        A
+He's ready to win
+       E    C
+On the hunt tonight
+    E             A
+For love at first sting
+
+
+[Chorus]
+
+E         G A    
+Here I am,     
+C            D       E         G A C D
+Rock you like a hurricane
+E         G A    
+Here I am,     
+C            D       E         G A C D
+Rock you like a hurricane 
+
 """
+
+
+print transpose_song(song, scale, 2)
